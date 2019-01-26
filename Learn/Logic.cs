4fd965a6887844b4d49be2fede799c89a67c99e9
Olorunfemi Ajibulu <FzanyAjibs@gmail.com>
2019-01-26 16:25:15 +0100

    addded score

--- Learn/Logic.cs
+++ Learn/Logic.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Learn
 {
@@ -10,26 +6,54 @@ namespace Learn
     {
         public static void Commander()
         {
-            Console.WriteLine("Enter the age");
-            int.TryParse(Console.ReadLine(), out int age);
-            switch (age)
+            // Program to Print out grades of an Inputed score.
+            Console.WriteLine("Enter score");
+            int.TryParse(Console.ReadLine(), out int score);
+            //switch (score)
+            //{
+            //    case :
+            //        {
+            //            Console.WriteLine("I am young");
+            //            break;
+            //        }
+            //    case 50:
+            //        {
+            //            Console.WriteLine("I am prime");
+            //            break;
+            //        }
+            //    case 70:
+            //        {
+            //            Console.WriteLine("I am old");
+            //            break;
+            //        }
+            //}
+            if (score <=39)
             {
-                case 20:
-                    {
-                        Console.WriteLine("I am young");
-                        break;
-                    }
-                case 50:
-                    {
-                        Console.WriteLine("I am prime");
-                        break;
-                    }
-                case 70:
-                    {
-                        Console.WriteLine("I am old");
-                        break;
-                    }
+                Console.WriteLine("Result - F");
             }
+
+            else if (score <= 49)
+            {
+                Console.WriteLine(" Result - E");
+            }
+            else if (score <= 59)
+            {
+                Console.WriteLine("Result - C");
+            }
+            else if (score <= 69)
+            {
+                Console.WriteLine("Result -   B");
+            }
+            else if (score <= 79)
+            {
+                Console.WriteLine("Result -   A");
+            }
+
+            Console.WriteLine(score);
+
+
+
+
         }
     }
 }
